24e88515e86f4699255d62c4a2e9afd23b52ec2b
justsoso <425168464@qq.com>
2026-05-17 14:35:48 +0800

    fix

--- cmd/deploy/test.go
+++ cmd/deploy/test.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func test() {
-	projectpath := projectPath()
-	fmt.Println(projectpath)
+	wd, err := os.Getwd()
+	fmt.Println(wd, err)
+
+	exePath, err := os.Executable()
+	fmt.Println(exePath, err)
+
+	pc, file, line, ok := runtime.Caller(0)
+	fmt.Println(pc, file, line, ok)
+
+	root, ok := findProjectRoot(file)
+	if !ok {
+		fmt.Println("Failed to find project root")
+		return
+	}
+	fmt.Println("Project root:", root)
 }
