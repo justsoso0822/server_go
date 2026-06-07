2ad57141e69e89b654d29d8aa85dba07ff86d244
justsoso0822 <justsoso0822@outlook.com>
2026-06-07 16:31:52 +0800

    feat: support JSON body in request param collection

--- middleware/helper.go
+++ middleware/helper.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -21,9 +24,23 @@ func collectRequestParams(c *gin.Context) map[string]interface{} {
 		params[k] = strings.Join(v, ",")
 	}
 	if c.Request.Method == "POST" {
-		_ = c.Request.ParseForm()
-		for k, v := range c.Request.PostForm {
-			params[k] = strings.Join(v, ",")
+		if strings.Contains(c.ContentType(), "application/json") {
+			body, err := io.ReadAll(c.Request.Body)
+			if err == nil {
+				// 还原 body，供后续 handler 的 ShouldBindJSON 使用
+				c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
+				var jsonParams map[string]interface{}
+				if json.Unmarshal(body, &jsonParams) == nil {
+					for k, v := range jsonParams {
+						params[k] = v
+					}
+				}
+			}
+		} else {
+			_ = c.Request.ParseForm()
+			for k, v := range c.Request.PostForm {
+				params[k] = strings.Join(v, ",")
+			}
 		}
 	}
 	return params
